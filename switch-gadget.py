8f432d749ab9eae6fee8d0aa7c90525f55fd9116
Paul Kerchen <paul@whirlingchair.com>
2017-11-19 12:57:33 -0500

    Implemented selection via encoder.

--- switch-gadget.py
+++ switch-gadget.py
@@ -1,12 +1,31 @@
 import RPi.GPIO as GPIO
 import io
+import os
+import sys
 import time
 import subprocess
 
+this_script_dir=os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(this_script_dir, 'pyLCD'))
+
 MODE_SWITCH_SCRIPT = "/home/pi/switch-mode.sh"
 
-SWITCH_MODE_BUTTON_PIN = 7
+# TFT buttons
+SWITCH_MODE_BUTTON_PIN = 15
 SEND_PASSWORD_BUTTON_PIN = 11
+TFT_BUTTON_3_PIN = 16
+TFT_BUTTON_4_PIN = 13
+
+# Encoder inputs
+ENC_A_PIN = 40
+ENC_B_PIN = 38
+ENC_BUTTON_PIN = 35
+#ENC_BIT4_PIN = 36
+ENC_COMMON_PIN = 33
+ENC_QUAD_PINS = [ ENC_B_PIN, ENC_A_PIN ]
+
+CW_ORDER = [ 1, 3, 0, 2 ]
+CCW_ORDER = [ 2, 0, 3, 1 ]
 
 HID_USB_MODE = 1
 RNDIS_USB_MODE = 2
@@ -20,7 +39,30 @@ def setup_gpio():
     GPIO.setup(SEND_PASSWORD_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(SEND_PASSWORD_BUTTON_PIN, GPIO.RISING, bouncetime=200)
 
-def check_gpio(mode):
+    GPIO.setup(ENC_COMMON_PIN, GPIO.OUT)
+    GPIO.output(ENC_COMMON_PIN, GPIO.LOW)
+
+    GPIO.setup(ENC_A_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(ENC_A_PIN, GPIO.BOTH)
+
+    GPIO.setup(ENC_B_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(ENC_B_PIN, GPIO.BOTH)
+
+    GPIO.setup(ENC_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(ENC_BUTTON_PIN, GPIO.RISING, bouncetime=50)
+
+    #GPIO.setup(ENC_BIT4_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    #GPIO.add_event_detect(ENC_BIT4_PIN, GPIO.BOTH, bouncetime=50)
+
+def get_enc_value():
+    new_val = 0
+    for pin in ENC_QUAD_PINS:
+        new_val = new_val * 2 + (1 if GPIO.input(pin) else 0)
+    #print("Encoder value: " + str(new_val))
+
+    return new_val
+
+def check_gpio(mode, current_enc_value):
     new_mode = mode
     if GPIO.event_detected(SWITCH_MODE_BUTTON_PIN):
         print('Mode switch button pressed')
@@ -39,9 +81,26 @@ def check_gpio(mode):
         else:
             print('Send password button pressed but not in HID mode')
 
-    return new_mode
+    if GPIO.event_detected(ENC_BUTTON_PIN):
+        print('Encoder button pressed.')
+
+    encoder_changed = False
+    for pin in ENC_QUAD_PINS:
+        if GPIO.event_detected(pin):
+            #print("Event for pin {0} detected".format(pin))
+            encoder_changed = True
+
+    new_enc_value = current_enc_value
+    if encoder_changed:
+        new_enc_value = get_enc_value()
+
+    return new_mode, new_enc_value
 
 def set_usb_mode(mode):
+    if True:
+        print("NOT setting USB mode.")
+        return
+
     if mode == HID_USB_MODE:
         subprocess.call([MODE_SWITCH_SCRIPT, "hid"])
     elif mode == RNDIS_USB_MODE:
@@ -60,11 +119,22 @@ def main():
     setup_gpio()
 
     mode = RNDIS_USB_MODE
-    set_usb_mode(mode)
+    #set_usb_mode(mode)
+    enc_value = get_enc_value()
+    selection = 0
 
     try:
         while 1:
-            mode = check_gpio(mode)
+            mode, new_enc_value = check_gpio(mode, enc_value)
+            if new_enc_value != enc_value:
+                if new_enc_value == CW_ORDER[enc_value]:
+                    selection += 1
+                    print("New selection: " + str(selection))
+                elif new_enc_value == CCW_ORDER[enc_value]:
+                    selection -= 1
+                    print("New selection: " + str(selection))
+                enc_value = new_enc_value
+
     except KeyboardInterrupt:
         GPIO.cleanup()
 
