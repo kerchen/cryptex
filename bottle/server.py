94773eccc3a6b6e923ad6d54aece937634165243
Paul Kerchen <paul@whirlingchair.com>
2017-11-26 12:57:50 -0500

    Starting web interface

---
+++ bottle/server.py
@@ -0,0 +1,31 @@
+from bottle import get, post, request, route, run, static_file
+
+@get('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+def check_login(user, passwd):
+    if user == 'paul' and passwd == 'pass':
+        return True
+    return False
+
+@post('/login')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+#@route('/favicon.ico')
+#def server_static(filename):
+    #return static_file(filename, root='/home/pi/bottle/assets')
+
+run(host='0.0.0.0', port=8080, debug=True)
