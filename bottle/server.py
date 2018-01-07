08cc8087d17684cd72aa2c18a923b3852c9eea5a
Paul Kerchen <paul@whirlingchair.com>
2018-01-07 16:43:07 -0500

    Using HTTPS instead of HTTP. Yay!

--- bottle/server.py
+++ bottle/server.py
@@ -1,6 +1,18 @@
-from bottle import get, post, request, route, run, static_file
+from bottle import (get, post, route, response, run, redirect, request,
+                    static_file, ServerAdapter, default_app)
+from cheroot import wsgi
+from cheroot.ssl.builtin import BuiltinSSLAdapter
+import ssl
 
 import shared_cfg
+
+@route('/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root=".")
+
+@route('/')
+def default():
+    redirect("/index.html")
 
 @get('/login')
 def login():
@@ -29,9 +41,32 @@ def do_login():
     else:
         return "<p>Login failed.</p>"
 
+# Create our own sub-class of Bottle's ServerAdapter
+# so that we can specify SSL. Using just server='cherrypy'
+# uses the default cherrypy server, which doesn't use SSL
+class SSLCherryPyServer(ServerAdapter):
+    def run(self, handler):
+        server = wsgi.Server((self.host, self.port), handler)
+        server.ssl_adapter = BuiltinSSLAdapter('./certs/device.crt',
+                                               './certs/device.key')
+
+        # By default, the server will allow negotiations with extremely old
+        # protocols that are susceptible to attacks, so only allow TLSv1.2
+        server.ssl_adapter.context.options |= ssl.OP_NO_TLSv1
+        server.ssl_adapter.context.options |= ssl.OP_NO_TLSv1_1
+
+        try:
+            server.start()
+        finally:
+            server.stop()
+
 #@route('/favicon.ico')
 #def server_static(filename):
     #return static_file(filename, root='/home/pi/bottle/assets')
 
 def run_web_server(debug):
-    run(host='0.0.0.0', port=80, debug=debug, quiet=not debug)
+    run(host='0.0.0.0',
+        port=443,
+        debug=debug,
+        quiet=not debug,
+        server=SSLCherryPyServer)
