051920f51be9cec30698fb7743408a27dc68b912
Paul Kerchen <paul@whirlingchair.com>
2018-04-14 21:27:15 -0400

    Added roundtrip xml serialization test

--- ut/test_passwordStore.py
+++ ut/test_passwordStore.py
@@ -2,12 +2,23 @@ import os
 from unittest import TestCase
 from pw_store import Entry, EntryContainer, PasswordStore
 
+
 class TestPasswordStore(TestCase):
     def setUp(self):
-        with open(os.path.join("ut", "store.xml"), "rb") as f:
+        self.test_xml_filename = os.path.join("ut", "store.xml")
+        with open(self.test_xml_filename, "rb") as f:
             raw_xml = f.read()
         self.cut = PasswordStore(raw_xml)
 
     def test_get_root(self):
         root = self.cut.get_root()
         self.assertIsNotNone(root)
+
+    def test_roundtrip_xml_serialization(self):
+        serialized_xml = self.cut.serialize_to_xml()
+        serialized_xml = serialized_xml.replace("\n", "")
+        file_xml = ""
+        with open(self.test_xml_filename, "rb") as f:
+            for line in f:
+                file_xml += line.strip()
+        self.assertEqual(serialized_xml, file_xml)
